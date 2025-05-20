662e189875a31290dc0bab1398291aa8a9bd00de
Harsh Vardhan <hv09989@gmail.com>
2025-05-20 13:48:41 +0530

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -24,7 +24,7 @@ function HeroSection() {
           </p>
           <div className=" pt-4 opacity-0 animate-fade-in-delay-4">
             <a
-              href="#project
+              href="#projects
             "
               className="cosmic-button"
             >
